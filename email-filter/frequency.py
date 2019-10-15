add3b2fa6985974bae4429c315481325907284f5
Noah Fields <k9theassassin@gmail.com>
2019-10-15 15:34:24 -0400

    Push Imported version

--- email-filter/frequency.py
+++ email-filter/frequency.py
@@ -1,28 +1,11 @@
-# import re
-# import string
-import glob, os
-from collections import Counter
-
-#
-# frequency = {}
-# os.chdir("train-mails")
-# for file in glob.glob("*.txt"):
-#     with open(file, 'r') as f:
-#         for line in f:
-#             for word in line.split():
-#                 count = frequency.get(word, 0)
-#                 frequency[word] = count + 1
-#
-#
-#
-# frequency= sorted(frequency.items(), key = lambda x : x[1])[::-1]
-#
-
-# print(frequency)
-import os, string
+import os
 import numpy as np
 from collections import Counter
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+from sklearn.svm import SVC, NuSVC, LinearSVC
+from sklearn.metrics import confusion_matrix
 
+# Create a dictionary of words with its frequency
 def make_Dictionary(train_dir):
     emails = [os.path.join(train_dir, f) for f in os.listdir(train_dir)]
     all_words = []
@@ -33,7 +16,18 @@ def make_Dictionary(train_dir):
                     words = line.split()
                     all_words += words
 
-    return Counter(all_words)
+    dictionary = Counter(all_words)
+    list_to_remove = list(dictionary)
+    for item in list_to_remove:
+        # if item.isalpha() == False:
+        #     del dictionary[item]
+        # el
+        if len(item) == 1:
+            del dictionary[item]
+    dictionary = dictionary.most_common(3000)
+    # Paste code for non-word removal here(code snippet is given below)
+    return dictionary
+
 
 def extract_features(mail_dir):
     files = [os.path.join(mail_dir,fi) for fi in os.listdir(mail_dir)]
@@ -53,43 +47,30 @@ def extract_features(mail_dir):
         docID = docID + 1
     return features_matrix
 
-# Create a dictionary of words with its frequency
 
 train_dir = 'train-mails'
 dictionary = make_Dictionary(train_dir)
 
-list_to_remove = list(dictionary)
-for item in list_to_remove:
-    # if item.isalpha() == False and item not in string.punctuation:
-    #     print(item)
-    #     del dictionary[item]
-    # el
-    if len(item) == 1:
-        del dictionary[item]
-dictionary = dictionary.most_common(3000)
-
 # Prepare feature vectors per training mail and its labels
+
 
 train_labels = np.zeros(702)
 train_labels[351:701] = 1
 train_matrix = extract_features(train_dir)
-print(train_labels, train_matrix)
-print(dictionary)
 
-# # Training SVM and Naive bayes classifier
-#
-# model1 = MultinomialNB()
-# model2 = LinearSVC()
-# model1.fit(train_matrix, train_labels)
-# model2.fit(train_matrix, train_labels)
-#
-# # Test the unseen mails for Spam
-# test_dir = 'test-mails'
-# test_matrix = extract_features(test_dir)
-# test_labels = np.zeros(260)
-# test_labels[130:260] = 1
-# result1 = model1.predict(test_matrix)
-# result2 = model2.predict(test_matrix)
-# print(confusion_matrix(test_labels, result1))
-# print(confusion_matrix(test_labels, result2))
+# Training SVM and Naive bayes classifier
 
+model1 = MultinomialNB()
+model2 = LinearSVC()
+model1.fit(train_matrix, train_labels)
+model2.fit(train_matrix, train_labels)
+
+# Test the unseen mails for Spam
+test_dir = 'test-mails'
+test_matrix = extract_features(test_dir)
+test_labels = np.zeros(260)
+test_labels[130:260] = 1
+result1 = model1.predict(test_matrix)
+result2 = model2.predict(test_matrix)
+print(confusion_matrix(test_labels, result1))
+print(confusion_matrix(test_labels, result2))
