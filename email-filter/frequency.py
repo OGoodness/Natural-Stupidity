9f29d956105328283b764df13d1647b85363724f
Reece <rcwienandt@gmail.com>
2019-10-20 11:58:27 -0400

    current

--- email-filter/frequency.py
+++ email-filter/frequency.py
@@ -9,44 +9,41 @@ from sklearn.metrics import confusion_matrix
 def make_Dictionary(train_dir):
     emails = [os.path.join(train_dir, f) for f in os.listdir(train_dir)]
     all_words = []
+    count = 0
     for mail in emails:
         with open(mail) as m:
             for i, line in enumerate(m):
-                if i == 2:  # Body of email is only 3rd line of text file
                     words = line.split()
                     all_words += words
 
     dictionary = Counter(all_words)
     list_to_remove = list(dictionary)
     for item in list_to_remove:
-        # if item.isalpha() == False:
-        #     del dictionary[item]
-        # el
+        if item == "Subject:":
+            del dictionary[item]
         if len(item) == 1:
             del dictionary[item]
-    dictionary = dictionary.most_common(100)
+    dictionary = dictionary.most_common(SIZE)
     # Paste code for non-word removal here(code snippet is given below)
     return dictionary
 
 
 def extract_features(mail_dir):
-    files = [os.path.join(mail_dir, fi) for fi in os.listdir(mail_dir)]
-    features_matrix = np.ones((len(files), 100))
+    files = [os.path.join(mail_dir,fi) for fi in os.listdir(mail_dir)]
+    features_matrix = np.ones((len(files), SIZE))
     docID = 0
     for fil in files:
-        with open(fil) as fi:
-            for i, line in enumerate(fi):
-                if i == 2:
-                    words = line.split()
-                    for word in words:
-                        wordID = 0
-                        for i, d in enumerate(dictionary):
-                            if d[0] == word:
-                                wordID = i
-                                features_matrix[docID, wordID] = words.count(word)
-            docID = docID + 1
+      with open(fil) as fi:
+        for i,line in enumerate(fi):
+            words = line.split()
+            for word in words:
+              wordID = 0
+              for i,d in enumerate(dictionary):
+                if d[0] == word:
+                  wordID = i
+                  features_matrix[docID,wordID] = words.count(word)
+        docID = docID + 1
     return features_matrix
-
 
 def test_data(test_dir, spam_count, ham_count, spam_dict, ham_dict):
     files = [os.path.join(test_dir, fi) for fi in os.listdir(test_dir)]
@@ -60,32 +57,37 @@ def test_data(test_dir, spam_count, ham_count, spam_dict, ham_dict):
                 words = line.split()
                 for word in words:
                     if word in spam_dict:
-                        spam = spam + spam_dict[word] * words.count(word) + 1.0 / 2.0
-                        ham = ham + ham_dict[word] * words.count(word) + 1.0 / 2.0
+                        spam = spam + spam_dict[word] * test[word] + np.log(1.0 / 2.0)
+                        ham = ham + ham_dict[word] * test[word] + np.log(1.0 / 2.0)
                     else:
-                        spam = spam + 1.0 / 2.0
-                        ham = ham + 1.0 / 2.0
+                        spam = spam + (1/(351 + 3000)) + np.log(1.0 / 2.0)
+                        ham = ham + (1/(351 + 3000)) + np.log(1.0 / 2.0)
             if spam > ham:
                 is_spam += [True]
+            else:
+                is_spam += [False]
             file_index += 1
     return is_spam
 
-
+SIZE = 3000
 train_dir = 'train-mails'
-test_dir = 'test-mails'
-dictionary = make_Dictionary(train_dir)
+small_test = 'small-test'
+test_mails = 'test-mails'
+dictionary = make_Dictionary(small_test)
+test = dict(dictionary)
 
 # Prepare feature vectors per training mail and its labels
 
 
-class_identifier = np.zeros(702)
-class_identifier[351:702] = 1
+class_identifier = np.zeros(SIZE)
+class_identifier[352:702] = 1
 train_matrix = extract_features(train_dir)
 
-# total_num_occurance = np.sum(train_matrix, axis = 0)
-ham_num_occurance = np.sum(train_matrix[0:351], axis=0)
-spam_num_occurance = np.sum(train_matrix[351:702], axis=0)
-process = lambda x: np.log(x / (451))
+
+#total_num_occurance = np.sum(train_matrix, axis = 0)
+ham_num_occurance = np.sum(train_matrix[0:351], axis = 0)
+spam_num_occurance = np.sum(train_matrix[352:702], axis = 0)
+process = lambda x: np.log((x/(351 + 3000)))
 spam_frequency = process(spam_num_occurance)
 ham_frequency = process(ham_num_occurance)
 
@@ -96,20 +98,29 @@ for iter, freq in enumerate(dictionary):
     spam_dictionary[freq[0]] = spam_frequency[iter]
     ham_dictionary[freq[0]] = ham_frequency[iter]
 
-spam_count = {}
-ham_count = {}
-for iter, freq in enumerate(dictionary):
-    spam_count[freq[0]] = spam_num_occurance[iter]
-    ham_count[freq[0]] = ham_num_occurance[iter]
+#print(ham_frequency)
+#print(ham_dictionary)
+#print(spam_frequency)
+#print(spam_dictionary)
+#results = finalCalculations(small_test)
 
-print(ham_frequency)
-print(ham_dictionary)
-print(spam_frequency)
-print(spam_dictionary)
+is_spam = test_data(test_mails, 130, 130, spam_dictionary, ham_dictionary)
 
-is_spam = test_data(test_dir, spam_count, ham_count, spam_dictionary, ham_dictionary)
+ham = 0
+spam = 0
+for value in is_spam:
+    if value == True:
+        spam += 1
+    else:
+        ham += 1
 
-print("test")
+print(spam)
+print(ham)
+print("Accuracy: " + str((260-abs(spam-ham))/260))
+
+
+
+
 # # Training Naive bayes classifier
 #
 # model1 = MultinomialNB()
