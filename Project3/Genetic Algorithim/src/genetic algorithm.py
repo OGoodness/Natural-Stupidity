e53b1f44d179905b73436dc4b9a4752136adbe81
Weston Rainer <Rainerw1@mymail.nku.edu>
2019-11-24 14:07:43 -0500

    Added a mutation for assignment operations

--- Project3/Genetic Algorithim/src/genetic algorithm.py
+++ Project3/Genetic Algorithim/src/genetic algorithm.py
@@ -18,54 +18,96 @@ import random
 
 def mutate(code):
     codeArray = code.splitlines()
+    codeArray = change_order(codeArray)
+    codeArray = ChangeAssignmentOperation(codeArray)
+
+    return '\n'.join(str(e) for e in codeArray)
+
+
+    for i in range(0, len(codeArray)):
+        assignmentCheck = codeArray[i].split(" ")
+        if len(assignmentCheck) > 1:
+            if assignmentCheck[1] == "=":
+                decider = random.randint(0, 11)
+                if decider == 0:
+                    codeArray[i] = codeArray[i].replace("+", "*")
+                if decider == 1:
+                    codeArray[i] = codeArray[i].replace("+", "/")
+                if decider == 2:
+                    codeArray[i] = codeArray[i].replace("+", "-")
+                if decider == 3:
+                    codeArray[i] = codeArray[i].replace("-", "*")
+                if decider == 4:
+                    codeArray[i] = codeArray[i].replace("-", "/")
+                if decider == 5:
+                    codeArray[i] = codeArray[i].replace("-", "+")
+                if decider == 6:
+                    codeArray[i] = codeArray[i].replace("/", "*")
+                if decider == 7:
+                    codeArray[i] = codeArray[i].replace("/", "+")
+                if decider == 8:
+                    codeArray[i] = codeArray[i].replace("/", "-")
+                if decider == 9:
+                    codeArray[i] = codeArray[i].replace("*", "+")
+                if decider == 10:
+                    codeArray[i] = codeArray[i].replace("*", "/")
+                if decider == 11:
+                    codeArray[i] = codeArray[i].replace("*", "-")
+
+    return codeArray
+
+
+def change_order(codeArray):
     swapOne = random.randint(0, len(codeArray) - 1)
     swapTwo = random.randint(0, len(codeArray) - 1)
     temp = codeArray[swapOne]
     codeArray[swapOne] = codeArray[swapTwo]
     codeArray[swapTwo] = temp
-
-    return '\n'.join(str(e) for e in codeArray)
+    return codeArray
 
 
 # noinspection PyUnresolvedReferences
-def fitness(code):
+def fitness(code, anwser):
     score = 0
     arrayTest = []
-    answer = []
     try:
         arrayTest = makeArray()
-        answer.append(0)
-        answer.append(1)
-        answer.append(2)
-        answer.append(3)
-        answer.append(4)
-        answer.append(5)
-        answer.append(6)
-        answer.append(7)
-        answer.append(8)
-        answer.append(9)
+
     except:
         print("Unexpected error:")
         score = -1
-    for i in range(0, len(arrayTest) - 1):
+    for i in range(0, len(arrayTest)):
         if arrayTest[i] == answer[i]:
             score += 1
     return score
 
 
+# code = """def makeArray():
+#     array = []
+#     array.append(0)
+#     array.append(1)
+#     array.append(2)
+#     array.append(3)
+#     array.append(4)
+#     array.append(5)
+#     array.append(6)
+#     array.append(7)
+#     array.append(8)
+#     array.append(9)
+#     return array"""
+
 code = """def makeArray():
-    array = []
-    array.append(0)
-    array.append(1)
-    array.append(2)
-    array.append(3)
-    array.append(4)
-    array.append(5)
-    array.append(6)
-    array.append(7)
-    array.append(8)
-    array.append(9)
-    return array"""
+\tarray = []
+\tx = 0 + 1
+\tarray.append(x)
+\tx = 2 - 2
+\tarray.append(x)
+\tx = 4 * 2
+\tarray.append(x)
+\treturn array"""
+
+answer = [0, 4, 2]
+
 
 codes = []
 
@@ -79,18 +121,18 @@ offs_per_pop = 6
 steps = 10
 score = 0
 
-while score != 10:
+while score != 3:
     for i in range(0, len(codes)):
         codes[i] = mutate(codes[i])
         try:
             exec(codes[i])
         except:
             print("Doesn't compile")
-        score = fitness(codes[i])
+        score = fitness(codes[i], answer)
         print(score)
 
-
-        if score == 10:
+#This is just a test break
+        if score == 3:
             break
 for x in codes:
     print(x)
