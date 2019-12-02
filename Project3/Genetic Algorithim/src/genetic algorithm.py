d2dacb752e758fb22dc07506860ee68f515b6f95
Noah <K9theassassin@gmail.com>
2019-12-02 18:24:49 -0500

    Added the crossover and replace variable functions

--- Project3/Genetic Algorithim/src/genetic algorithm.py
+++ Project3/Genetic Algorithim/src/genetic algorithm.py
@@ -1,27 +1,85 @@
 import random
+import re
+from operator import itemgetter
+from time import sleep
 
 
-# def makeArray():
-#   array = []
-#    array.append(0)
-#    array.append(1)
-#    array.append(2)
-#    array.append(3)
-#    array.append(4)
-#    array.append(5)
-#    array.append(6)
-#    array.append(7)
-#    array.append(8)
-#    array.append(9)
-#    return array
+def categorize_variables(codeArray):
+    variables = {"string": [],
+                 "number": [],
+                 "variable": [],
+                 "array": []
+                }
+    for line in codeArray:
+        if '=' in line:
+            line = re.sub('\s+', '', line)
+            variable_name, value = line.split('=')
+
+            string_rules = ['\'' in value,
+                            '"' in value,
+                            '\'' in value,
+                            value not in variables["string"]]
+            number_rules = [bool(re.search(r'\d', value)) == True,
+                            not all(string_rules),
+                            value not in variables["number"]
+                            ]
+            array_rules = [value[0] == '[',
+                           value not in variables["array"]
+                           ]
+            variable_rules = [not all(string_rules),
+                              bool(re.search('[a-zA-Z]', value)) == True,
+                              value not in variables["variable"]
+                              ]
+            if all(number_rules):
+                variables["number"].append(variable_name)
+            elif all(string_rules):
+                variables["string"].append(variable_name)
+            elif all(variable_rules):
+                variables["variable"].append(variable_name)
+            elif all(array_rules):
+                variables["array"].append(variable_name)
+
+    return variables
+def swap_variables(codeArray):
+    variables = categorize_variables(codeArray)
+    output = []
+    for line in codeArray:
+        decider = random.randint(0, 1)
+        if decider >= 1:
+            for key, value in variables.items():
+                if any(var in value for var in line):
+                    matching = [s for s in value if s in line]
+                    for var in matching:
+                        line = line.replace(var, random.choice(value))
+        output.append(line)
+    return output
 
 
 def mutate(code):
     codeArray = code.splitlines()
     codeArray = change_order(codeArray)
+    codeArray = swap_variables(codeArray)
     codeArray = ChangeAssignmentOperation(codeArray)
-
     return '\n'.join(str(e) for e in codeArray)
+
+
+def Crossover(parents):
+    previous = ''
+    genes = {"top": [], "bottom": [] }
+    children = []
+    for parent in parents:
+        if parent != previous:
+            previous = parent
+            end = parent.count('\n')+1
+            middle = int(end / 2)
+            genes['top'].append(parent.splitlines()[0:middle])
+            genes['bottom'].append(parent.splitlines()[middle:end])
+    for r in range(0, len(genes["top"])):
+        for j in range(0, len(genes["bottom"])):
+            if r != j:
+                children.append('\n'.join(genes["top"][r] + genes["bottom"][j]))
+    return children
+
 
 
 def ChangeAssignmentOperation(codeArray):
@@ -73,42 +131,28 @@ def fitness(code, anwser):
     arrayTest = []
     try:
         arrayTest = makeArray()
-
+        for i in range(0, len(arrayTest)):
+            if arrayTest[i] == answer[i]:
+                score += 1
     except:
         print("Unexpected error:")
         score = -1
-    for i in range(0, len(arrayTest)):
-        if arrayTest[i] == answer[i]:
-            score += 1
     return score
 
 
-# code = """def makeArray():
-#     array = []
-#     array.append(0)
-#     array.append(1)
-#     array.append(2)
-#     array.append(3)
-#     array.append(4)
-#     array.append(5)
-#     array.append(6)
-#     array.append(7)
-#     array.append(8)
-#     array.append(9)
-#     return array"""
 
 code = """def makeArray():
 \tarray = []
 \tx = 0 + 1
+\ty = x1 + 1
 \tarray.append(x)
-\tx = 2 - 2
-\tarray.append(x)
+\tz = 2 - 2
+\tx.append(x)
 \tx = 4 * 2
 \tarray.append(x)
 \treturn array"""
 
 answer = [0, 4, 2]
-
 
 codes = []
 
@@ -118,22 +162,29 @@ codes.append(code)
 codes.append(code)
 codes.append(code)
 
-offs_per_pop = 6
+offspring_per_pop = 6
 steps = 10
-score = 0
-
-while score != 3:
+highest_score = 0
+while highest_score != 3:
+    highest_score = 0
+    ranks = {}
     for i in range(0, len(codes)):
+        score = 0
         codes[i] = mutate(codes[i])
         try:
             exec(codes[i])
+            score = fitness(codes[i], answer)
+            print("Compiles")
         except:
             print("Doesn't compile")
-        score = fitness(codes[i], answer)
-        print(score)
-
-#This is just a test break
+        ranks[i] = score
+        if score >= highest_score:
+            highest_score = score
         if score == 3:
             break
+    top_3 = sorted(ranks.keys(), key=ranks.get, reverse=True)[:3]
+
+    #Need to decide which ones get sent into crossover
+    codes = Crossover(list(itemgetter(*top_3)(codes)))
 for x in codes:
     print(x)
